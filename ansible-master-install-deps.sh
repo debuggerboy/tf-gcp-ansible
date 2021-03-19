e05f769b1acd5c28f4f2ff1e1a7b9b4b1abde856
debuggerboy <debuggerboy@cassandra.pulsiot.com>
2021-03-20 01:18:41 +0530

    updates - master install pkg dependencies and slave firewall changes

--- ansible-master-install-deps.sh
+++ ansible-master-install-deps.sh
@@ -2,6 +2,6 @@ sudo curl -sSO https://dl.google.com/cloudagents/add-monitoring-agent-repo.sh &&
 sudo echo 'deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main' > /etc/apt/sources.list.d/ansible-debian.list 2>/dev/null
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 sudo apt-get update
-sudo apt-get install -yq python3 python3-pip python3-venv ansible stackdriver-agent
+sudo apt-get install -yq python3 python3-pip python3-venv ansible stackdriver-agent git
 sudo rm add-monitoring-agent-repo.sh
 sudo chmod 600 /home/anish/.ssh/id_rsa
--- ansible-slaves.tf
+++ ansible-slaves.tf
@@ -45,7 +45,7 @@ resource "google_compute_firewall" "slave" {
 
  allow {
    protocol = "tcp"
-   ports    = ["22"]
+   ports    = ["22", "80", "443"]
  }
 }
 
